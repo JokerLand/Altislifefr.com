ef15120e1ebc72ce6afcb39ead8022689cbe642a
kive3651 <jefolliot@laposte.net>
2014-10-15 18:49:00 +0200

    Maj tonic dialog1
    Mise ajour du pInteraction.h

--- Update_Version_Tonic/extDB-Build/Altis_Life.Altis/dialog/pInteraction.h
+++ Update_Version_Tonic/extDB-Build/Altis_Life.Altis/dialog/pInteraction.h
@@ -116,7 +116,7 @@ class pInteraction_Menu
 			h = 0.038;
 		};
 		
-		class ButtonSix : life_RscButtonMenu
+				class ButtonSix : life_RscButtonMenu
 		{
 			idc = 37455;
 			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
@@ -136,6 +136,53 @@ class pInteraction_Menu
 			sizeEx = 0.025;
 			x = BGX + 0.03;
 			y = BGY + 0.37;
+			w = 0.24;
+			h = 0.038;
+		};
+		class ButtonEight : life_RscButtonMenu
+		{
+			idc = 37457;
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+			text = "";
+			sizeEx = 0.025;
+			x = BGX + 0.03;
+			y = BGY + 0.42;
+			w = 0.24;
+			h = 0.038;
+		};
+		
+		class ButtonNine : life_RscButtonMenu
+		{
+			idc = 37458;
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+			text = "";
+			sizeEx = 0.025;
+			x = BGX + 0.03;
+			y = BGY + 0.47;
+			w = 0.24;
+			h = 0.038;
+		};
+		
+		class ButtonEleven : life_RscButtonMenu
+		{
+			idc = 37459;
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+			text = "";
+			sizeEx = 0.025;
+			x = BGX + 0.03;
+			y = BGY + 0.52;
+			w = 0.24;
+			h = 0.038;
+		};
+		
+		class ButtonTwelve : life_RscButtonMenu
+		{
+			idc = 37460;
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+			text = "";
+			sizeEx = 0.025;
+			x = BGX + 0.03;
+			y = BGY + 0.57;
 			w = 0.24;
 			h = 0.038;
 		};
