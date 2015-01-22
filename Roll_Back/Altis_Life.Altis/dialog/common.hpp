54f471c3b4dc59f782ee1198f2567bcfcc25b89e
JokerLand <mathieu.steenput@gmail.com>
2015-01-22 15:36:36 +0100

    #Hotfix affichage magasin

--- Roll_Back/Altis_Life.Altis/dialog/common.hpp
+++ Roll_Back/Altis_Life.Altis/dialog/common.hpp
@@ -485,41 +485,44 @@ class Life_RscProgress
 class Life_RscListBox 
 {
 	style = 16;
-	idc = -1;
-	type = 5;
-	w = 0.275;
-	h = 0.04;
-	font = "PuristaMedium";
-	colorSelect[] = {1, 1, 1, 1};
-	colorText[] = {1, 1, 1, 1};
-	colorBackground[] = {0.28,0.28,0.28,0.28};
-	colorSelect2[] = {1, 1, 1, 1};
-	colorSelectBackground[] = {0.95, 0.95, 0.95, 0.5};
-	colorSelectBackground2[] = {1, 1, 1, 0.5};
-	colorScrollbar[] = {0.2, 0.2, 0.2, 1};
-	arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
-	arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
-	wholeHeight = 0.45;
-	rowHeight = 0.04;
-	color[] = {0.7, 0.7, 0.7, 1};
-	colorActive[] = {0,0,0,1};
-	colorDisabled[] = {0,0,0,0.3};
-	sizeEx = 0.023;
-	soundSelect[] = {"",0.1,1};
-	soundExpand[] = {"",0.1,1};
-	soundCollapse[] = {"",0.1,1};
-	maxHistoryDelay = 1;
-	autoScrollSpeed = -1;
-	autoScrollDelay = 5;
-	autoScrollRewind = 0;
-	tooltipColorText[] = {1,1,1,1};
-	tooltipColorBox[] = {1,1,1,1};
-	tooltipColorShade[] = {0,0,0,0.65};
-	class ListScrollBar: Life_RscScrollBar
-	{
-		color[] = {1,1,1,1};
-		autoScrollEnabled = 1;
-	};
+    idc = -1;
+    type = 5;
+    w = 0.275;
+    h = 0.04;
+    font = "PuristaMedium";
+    colorSelect[] = {1, 1, 1, 1};
+    colorText[] = {1, 1, 1, 1};
+    colorBackground[] = {0.28,0.28,0.28,0.28};
+    colorSelect2[] = {1, 1, 1, 1};
+    colorSelectBackground[] = {0.95, 0.95, 0.95, 0.5};
+    colorSelectBackground2[] = {1, 1, 1, 0.5};
+    colorScrollbar[] = {0.2, 0.2, 0.2, 1};
+    colorPicture[] = {1,1,1,1};
+    colorPictureSelected[] = {1,1,1,1};
+    colorPictureDisabled[] = {1,1,1,1};
+    arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+    arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+    wholeHeight = 0.45;
+    rowHeight = 0.04;
+    color[] = {0.7, 0.7, 0.7, 1};
+    colorActive[] = {0,0,0,1};
+    colorDisabled[] = {0,0,0,0.3};
+    sizeEx = 0.023;
+    soundSelect[] = {"",0.1,1};
+    soundExpand[] = {"",0.1,1};
+    soundCollapse[] = {"",0.1,1};
+    maxHistoryDelay = 1;
+    autoScrollSpeed = -1;
+    autoScrollDelay = 5;
+    autoScrollRewind = 0;
+    tooltipColorText[] = {1,1,1,1};
+    tooltipColorBox[] = {1,1,1,1};
+    tooltipColorShade[] = {0,0,0,0.65};
+    class ListScrollBar: Life_RscScrollBar
+    {
+        color[] = {1,1,1,1};
+        autoScrollEnabled = 1;
+    };
 };
 
 class Life_RscEdit {
