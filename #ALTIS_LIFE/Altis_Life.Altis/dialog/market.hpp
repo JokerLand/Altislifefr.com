43715dce6572a13dae68ef9a499f2ea76951ca98
JokerLand <mathieu.steenput@gmail.com>
2015-03-17 01:27:23 +0100

    Market  3

--- #ALTIS_LIFE/Altis_Life.Altis/dialog/market.hpp
+++ #ALTIS_LIFE/Altis_Life.Altis/dialog/market.hpp
@@ -17,7 +17,7 @@ class marketView
 		class RscStructuredText_1100: RscStructuredText
 		{
 			idc = 1100;
-			text = "<t size='1.2' align='center'>Altis Markt</t>"; //--- ToDo: Localize;
+			text = "<t size='1.2' align='center'>Marché Altis</t>"; //--- ToDo: Localize;
 			x = 5 * GUI_GRID_W + GUI_GRID_X;
 			y = 1 * GUI_GRID_H + GUI_GRID_Y;
 			w = 30 * GUI_GRID_W;
@@ -35,7 +35,7 @@ class marketView
 		class lblResourceName: RscText
 		{
 			idc = 1000;
-			text = "Resource"; //--- ToDo: Localize;
+			text = "Ressources"; //--- ToDo: Localize;
 			x = 16.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 18 * GUI_GRID_W;
@@ -72,7 +72,7 @@ class marketView
 		class RscStructuredText_1101: RscStructuredText
 		{
 			idc = 1101;
-			text = "<t align='center'>Entwicklung</t>"; //--- ToDo: Localize;
+			text = "<t align='center'>développement</t>"; //--- ToDo: Localize;
 			x = 17 * GUI_GRID_W + GUI_GRID_X;
 			y = 9 * GUI_GRID_H + GUI_GRID_Y;
 			w = 17 * GUI_GRID_W;
@@ -100,7 +100,7 @@ class marketView
 		class RscText_1002: RscText
 		{
 			idc = 1002;
-			text = "Seit Serverstart"; //--- ToDo: Localize;
+			text = "Depuis le serveur est démarré"; //--- ToDo: Localize;
 			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 13 * GUI_GRID_W;
@@ -110,7 +110,7 @@ class marketView
 		class RscText_1003: RscText
 		{
 			idc = 1003;
-			text = "Vor kurzem"; //--- ToDo: Localize;
+			text = "récemment"; //--- ToDo: Localize;
 			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 13.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 13 * GUI_GRID_W;
@@ -142,7 +142,7 @@ class marketView
 		class bClose: RscButtonMenu
 		{
 			idc = 2400;
-			text = "Schliessen"; //--- ToDo: Localize;
+			text = "fermer"; //--- ToDo: Localize;
 			x = 5 * GUI_GRID_W + GUI_GRID_X;
 			y = 23 * GUI_GRID_H + GUI_GRID_Y;
 			w = 30 * GUI_GRID_W;
