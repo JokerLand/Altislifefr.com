0fb958696c9d5a381e273fc8d7f29a68d3a1b3ea
Sakzy <kitanovski@laposte.net>
2015-04-08 11:58:56 +0200

    #MAJ_Donator_Shop_config

--- #VERSION4.0/Altis_Life.Altis/Config_Shops.hpp
+++ #VERSION4.0/Altis_Life.Altis/Config_Shops.hpp
@@ -150,6 +150,90 @@ class WeaponShops {
 			{ "9Rnd_45ACP_Mag", "", 200 }
         };
     };
+    //Donateur Shops
+    class donator
+        name = "Donator Shop";
+        side = "donator";
+        license = "";
+        level = 3;
+        msg = "Vous devez etre donateur!";
+        items[] = {
+            { "hgun_Rook40_F", "", 8000 },
+            { "hgun_Pistol_heavy_01_F", "", 10000 },
+            { "R3F_PAMAS", "", 10000 },
+            { "hgun_PDW2000_F", "", 15000 },
+            { "SMG_01_F", "", 20000 },
+            { "arifle_Mk20C_plain_F", "", 100000 },
+            { "SIG_P226", "", 8000 },
+            { "C1987_MP7", "", 50000 },
+            { "C1987_MP7_ucp", "", 50000 },
+            { "C1987_MP7_urb", "", 50000 },
+            { "C1987_MP7_dmarpat", "", 50000 },
+            { "C1987_MP7_marpat", "", 50000 },
+            { "C1987_MP7_wcam", "", 50000 },
+            { "C1987_MP7_sand", "", 50000 },
+            { "C1987_MP7_nwu", "", 50000 },
+            { "C1987_MP7_folded_p", "", 50000 },
+            { "C1987_20Rnd_46x30_mp7", "", 100 },
+            { "C1987_40Rnd_46x30_mp7", "", 200 },
+            { "SG553", "", 150000 },
+            { "SG553_CQB", "", 150000 },
+            { "SG553_GL", "", 150000 },
+            { "LMG_Mk200_F", "", 200000 },
+            { "LMG_Zafir_F", "", 350000 },
+            { "srifle_LRR_camo_F", "", 400000 },
+            { "srifle_GM6_F", "", 400000 },
+            { "srifle_GM6_camo_F", "", 400000 },
+            { "R3F_FRF2", "", 250000 },
+            { "Trixie_M24_Clean", "", 300000 },
+            { "Trixie_M40A3_Clean", "", 300000 },
+            { "Trixie_CZ750", "", 350000 },
+            { "Trixie_AWM338", "", 400000 },
+            { "R3F_PGM_Hecate_II", "", 500000 },
+            { "Rangefinder", "", 50000 },
+            { "optic_LRPS", "", 50000 },
+            { "optic_SOS", "", 30000 },
+            { "optic_MRCO", "", 10000 },
+            { "optic_Arco", "", 10000 },
+            { "optic_Hamr", "", 10000 },
+            { "optic_ACO_grn_smg", "", 2000 },
+            { "optic_MRD", "", 1500 },
+            { "acc_pointer_IR", "", 10000 },
+            { "NVGoggles", "", 2000 },
+            { "ItemRadio", "", 500 },
+            { "ToolKit", "", 250 },
+            { "itemgps", "", 150 },
+            { "FirstAidKit", "", 150 },
+            { "30Rnd_56x45_GP90", "", 100 },
+            { "30Rnd_56x45_GP90_Armour_Piercing", "", 500 },
+            { "1Rnd_Smoke_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeRed_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeGreen_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeYellow_Grenade_shell", "", 100 },
+            { "1Rnd_SmokePurple_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeBlue_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeOrange_Grenade_shell", "", 100 },
+            { "1Rnd_SmokeOrange_Grenade_shell", "", 100 },
+            { "Cha_Pecheneg", "", 300000 },
+            { "Cha_100Rnd_762x54_PK", "", 100 },
+            { "Cha_100Rnd_762x54_PK_T", "", 100 },
+            { "16Rnd_9x21_Mag", "", 50 },
+            { "30Rnd_9x21_Mag", "", 60 },
+            { "R3F_15Rnd_9x19_PAMAS", "", 60 },
+            { "11Rnd_45ACP_Mag", "", 50 },
+            { "30Rnd_45ACP_Mag_SMG_01_tracer_green", "", 60 },
+            { "30Rnd_556x45_Stanag", "", 70 },
+            { "200Rnd_65x39_cased_Box_Tracer", "", 800 },
+            { "150Rnd_762x51_Box_Tracer", "", 800 },
+            { "7Rnd_408_Mag", "", 800 },
+            { "5Rnd_127x108_Mag", "", 800 },
+            { "R3F_10Rnd_762x51_FRF2", "", 800 },
+            { "Trixie_5x762_Mag", "", 800 },
+            { "Trixie_10x762_Mag", "", 800 },
+            { "Trixie_5x338_Mag", "", 800 },
+            { "R3F_7Rnd_127x99_PGM", "", 800 }   
+        };
+    };
     //Medic Shops
     class med_basic {
         name = "store";
