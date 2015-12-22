d9aba6ef6e94d4ac3f38fa33b8b50d4e0a53c5ce
Sakzy <kitanovski@laposte.net>
2015-12-23 00:34:50 +0100

    #5.0.0.1_Ajout_Menu_Tablette_Y_Version_ALFR

--- #Altis_Life_Version_5/missionv5/Altis_Life.Altis/dialog/admin_menu.hpp
+++ #Altis_Life_Version_5/missionv5/Altis_Life.Altis/dialog/admin_menu.hpp
@@ -6,22 +6,24 @@ class life_admin_menu {
 	onLoad = "[] spawn life_fnc_adminMenu;";
 	
 	class controlsBackground {
-		class Life_RscTitleBackground:Life_RscText {
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
-			idc = -1;
-			x = 0.1;
-			y = 0.2;
-			w = 0.8;
-			h = (1 / 25);
+	
+	    class fondtablet: Life_RscPicture
+		{
+			idc = 9090909;
+			text = "textures\menu.paa";
+			x = 0;
+			y = -0.12;
+			w = 1;
+			h = 1.28;
 		};
 		
 		class MainBackground:Life_RscText {
-			colorBackground[] = {0, 0, 0, 0.7};
+			colorBackground[] = {0, 0, 0, 0.2};
 			idc = -1;
 			x = 0.1;
 			y = 0.2 + (11 / 250);
 			w = 0.8;
-			h = 0.6 - (2 / 250);
+			h = 0.6 - (22 / 250);
 		};
 	};
 	
@@ -64,8 +66,9 @@ class life_admin_menu {
 			idc = -1;
 			text = "$STR_Global_Close";
 			onButtonClick = "closeDialog 0;";
+			colorBackground[] = {0, 0, 0, 0.0};			
 			x = -0.06 + (6.25 / 40) + (1 / 250 / (safezoneW / safezoneH));
-			y = 0.88 - (1 / 25);
+			y = 0.8 - (1 / 25);
 			w = (6.25 / 40);
 			h = (1 / 25);
 		};
@@ -74,8 +77,9 @@ class life_admin_menu {
 			idc = -1;
 			text = "$STR_Admin_GetID";
 			onButtonClick = "[] call life_fnc_admingetID;";
+			colorBackground[] = {0, 0, 0, 0.0};			
 			x = 0.1 + (6.25 / 40) + (1 / 250 / (safezoneW / safezoneH));
-			y = 0.88 - (1 / 25);
+			y = 0.8 - (1 / 25);
 			w = (6.25 / 40);
 			h = (1 / 25);
 		};
