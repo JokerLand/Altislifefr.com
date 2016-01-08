48e1ecf43debdf74eb5495c82ea997920e1a0808
Sakzy <kitanovski@laposte.net>
2016-01-09 00:26:04 +0100

    #5.0_Fix_pomme

--- #ALFR_5.0/Altis_Life.Altis/Config_Master.hpp
+++ #ALFR_5.0/Altis_Life.Altis/Config_Master.hpp
@@ -163,7 +163,7 @@ class VirtualItems {
 	VITEMMACRO(waterBottle, "STR_Item_WaterBottle", "waterBottle", 1, 10, 1, false, 100, "icons\ico_waterBottle.paa")
 
 	//Food
-	VITEMMACRO(apple, "STR_Item_Apple", "apple", 1, 5, 2, false, 10, "icons\ico_apple.paa")
+	VITEMMACRO(apple, "STR_Item_Apple", "apple", 1, 5, 5, false, 10, "icons\ico_apple.paa")
 	VITEMMACRO(peach, "STR_Item_Peach", "peach", 1, 10, 5, false, 10, "icons\ico_peach.paa")
 	VITEMMACRO(tbacon, "STR_Item_TBacon", "tbacon", 1, 10, 2, false, 40, "icons\ico_tbacon.paa")
 	VITEMMACRO(donuts, "STR_Item_Donuts", "donuts", 1, 2, 1, false, 30, "icons\ico_donut.paa")
