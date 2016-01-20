a89f7ac74d63b2004c20477b902e606b490e2310
Sakzy <kitanovski@laposte.net>
2016-01-20 18:23:09 +0100

    #5.0_meme_radio_cop/east/medic

--- #ALFR_5.0/Altis_Life.Altis/Config_Shops.hpp
+++ #ALFR_5.0/Altis_Life.Altis/Config_Shops.hpp
@@ -247,7 +247,7 @@ class WeaponShops {
 			{ "Medikit", "", 50 },
 			{ "pmc_earpiece", "", 100 },
 			{ "Rangefinder", "", 15 },
-			{ "tf_anprc152_999", "Radio (5km)", 0 }
+			{ "tf_fadak_999", "Radio (5km)", 0 }
         };
     };
     //Armurerie policier
@@ -907,7 +907,7 @@ class WeaponShops {
             { "pmc_earpiece", "", 100 },
             { "H_PilotHelmetHeli_B", "", 80 },
 			{ "ALFR_Police_Backpack", "", 100 },
-            { "tf_anprc152", "Radio (5km)", 0 },
+            { "tf_fadak_999", "Radio (5km)", 0 },
             { "tf_anprc155_coyote", "Radio (20km)(sac)", 50 },
             { "tf_anarc164", "Radio (40km)(sac)", 50 }
         };
