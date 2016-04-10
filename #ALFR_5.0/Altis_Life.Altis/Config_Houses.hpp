101ebb1ad56dbedcec853548e04fc85a622a9940
Artkyom <2axel4431@gmail.com>
2016-04-11 00:37:28 +0200

    #5.0_Coffre_maison

--- #ALFR_5.0/Altis_Life.Altis/Config_Houses.hpp
+++ #ALFR_5.0/Altis_Life.Altis/Config_Houses.hpp
@@ -1,7 +1,7 @@
 class Houses {
 	class Land_i_House_Big_02_V1_F {
 		price = 610000;
-		maxStorage = 1;
+		maxStorage = 3;
 		alarm = true;
 	};
 	class Land_i_House_Big_02_V2_F : Land_i_House_Big_02_V1_F{};
@@ -9,7 +9,7 @@ class Houses {
 
 	class Land_i_House_Big_01_V1_F {
 		price = 650000;
-		maxStorage = 1;
+		maxStorage = 4;
 		alarm = true;
 	};
 	class Land_i_House_Big_01_V2_F : Land_i_House_Big_01_V1_F{};
@@ -17,30 +17,30 @@ class Houses {
 
 	class Land_i_House_Small_01_V1_F {
 		price = 490000;
-		maxStorage = 1;
-		alarm = false;
+		maxStorage = 2;
+		alarm = true;
 	};
 	class Land_i_House_Small_01_V2_F : Land_i_House_Small_01_V1_F{};
 	class Land_i_House_Small_01_V3_F : Land_i_House_Small_01_V1_F{};
 
 	class Land_i_House_Small_02_V1_F {
 		price = 490000;
-		maxStorage = 1;
-		alarm = false;
+		maxStorage = 2;
+		alarm = true;
 	};
 	class Land_i_House_Small_02_V2_F : Land_i_House_Small_02_V1_F{};
 	class Land_i_House_Small_02_V3_F : Land_i_House_Small_02_V1_F{};
 
 	class Land_i_House_Small_03_V1_F {
 		price = 570000;
-		maxStorage = 1;
+		maxStorage = 3;
 		alarm = true;
 	};
 
 	class Land_i_Stone_HouseSmall_V2_F {
 		price = 410000;
 		maxStorage = 1;
-		alarm = false;
+		alarm = true;
 	};
 
 	class Land_i_Stone_HouseSmall_V1_F : Land_i_Stone_HouseSmall_V2_F{};
