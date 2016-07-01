cba4629a8e8a1c6aae14b79f886d59a9b243335b
Sakzy <kitanovski@laposte.net>
2016-07-01 15:23:52 +0200

    #5.5_Rajout_Menu_Spawn_version_alfr
    -Bien de rajouter le JPG mais sans le script c'est useless

--- #ALFR_5.5/Altis_Life.Altis/dialog/spawnSelection.hpp
+++ #ALFR_5.5/Altis_Life.Altis/dialog/spawnSelection.hpp
@@ -1,81 +1,120 @@
-class life_spawn_selection {
-    idd = 38500;
-    movingEnable = 0;
-    enableSimulation = 1;
-
-    class controlsBackground {
-        class life_RscTitleBackground: Life_RscText    {
-            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
-            idc = -1;
-            x = 0.1;
-            y = 0.2;
-            w = 0.8;
-            h = (1 / 25);
-        };
-
-        class MainBackground: Life_RscText {
-            colorBackground[] = {0,0,0,0.7};
-            idc = -1;
-            x = 0.1;
-            y = 0.2 + (11 / 250);
-            w = 0.8;
-            h = 0.6 - (22 / 250);
-        };
-
-        class Title: Life_RscTitle {
-            colorBackground[] = {0,0,0,0};
-            idc = -1;
-            text = "$STR_Spawn_Title";
-            x = 0.1;
-            y = 0.2;
-            w = 0.8;
-            h = (1 / 25);
-        };
-
-        class SpawnPointTitle: Title {
-            idc = 38501;
-            style = 1;
-            text = "";
-        };
-
-        class MapView : Life_RscMapControl {
-            idc = 38502;
-            x = 0.328;
-            y = 0.26;
-            w = 0.56;
-            h = 0.56 - (22 / 250);
-            maxSatelliteAlpha = 0.75;//0.75;
-            alphaFadeStartScale = 1.15;//0.15;
-            alphaFadeEndScale = 1.29;//0.29;
-        };
-    };
-
-    class controls {
-        class SpawnPointList: Life_RscListNBox {
-            idc = 38510;
-            text = "";
-            sizeEx = 0.041;
-            coloumns[] = {0,0,0.9};
-            drawSideArrows = 0;
-            idcLeft = -1;
-            idcRight = -1;
-            rowHeight = 0.050;
-            x = 0.105;
-            y = 0.26;
-            w = (8.8 / 40);
-            h = (10 / 25);
-            onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
-        };
-
-        class spawnButton: Life_RscButtonMenu {
-            idc = -1;
-            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
-            text = "$STR_Spawn_Spawn";
-            onButtonClick = "[] call life_fnc_spawnConfirm";
-            x = 0.11;
-            y = 0.69;
-            w = (8 / 40);
-            h = (1 / 25);
-        };
-    };
+class life_spawn_selection
+{
+	idd = 38500;
+	movingEnabled = false;
+	enableSimulation = true;
+	
+	class controlsBackground
+	{
+		class Logo: Life_RscPicture
+		{
+			idc = 1200;
+			text = "icons\AltisLifefr.jpg";
+			x = 0.725;
+			y = 0.063;
+			w = 0.3;
+			h = 0.1;
+		};
+		class Title: Life_RscText
+		{
+			idc = 1000;
+			text = "Lieux de Spawn";
+			x = 0.00883839;
+			y = 0.033468;
+			w = 0.6875;
+			h = 0.12;
+			sizeEx = 0.07;
+		};
+		class MapView : Life_RscMapControl 
+		{
+			idc = 38502;
+			x = 0.255; 
+			y = 0.16;
+			w = 0.749;
+			h = 0.84;
+			maxSatelliteAlpha = 0.75;
+			alphaFadeStartScale = 1.15;
+			alphaFadeEndScale = 1.29;
+		};
+		class listBackground: Life_RscText
+		{
+			idc = 100000;
+			x = 0.025;
+			y = 0.158;
+			w = 0.230;
+			h = 0.84;
+			colorBackground[] = {0.08,0.08,0.08,1};
+			text = "   ";
+		};		
+		class buttonBackground: Life_RscText
+		{
+			idc = 100000;
+			x = 0.7035;
+			y = 1;
+			w = 0.3;
+			h = 0.1;
+			colorBackground[] = {0.17,0.17,0.17,1};
+			text = "   ";
+		};
+	};
+	
+	class controls
+	{	
+		class SpawnPointList: Life_RscListNBox
+		{
+			idc = 38510;
+			coloumns[] = {0,0,0.9};
+			drawSideArrows = false;
+			idcLeft = -1;
+			idcRight = -1;
+			rowHeight = 0.1;
+			sizeEx = 0.05;
+			colorSelectBackground[] = {0.17,0.17,0.17,1};
+			colorSelectBackground2[] = {0.17,0.17,0.17,1};
+			colorSelect[] = {1,1,1,1};
+			colorSelect2[] = {1,1,1,1};
+			x = 0.025;
+			y = 0.16;
+			w = 0.230;
+			h = 0.84;
+			onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
+		};
+		
+		class spawnButton : Life_RscButtonMenu
+		{
+			idc = -1;
+			text = "Se Réveiller";
+			x = 0.7035;
+			y = 0.999;
+			w = 0.3;
+			h = 0.1;
+			colorBackground[] = {0.08,0.08,0.08,1};
+			colorBackground2[] = {0.08,0.08,0.08,1};
+			colorBackgroundFocused[] = {0.17,0.17,0.17,1};
+			color[] = {1,1,1,1};
+			colorFocused[] = {1,1,1,1};
+			color2[] = {1,1,1,1};
+			colorText[] = {1,1,1,1};
+			onButtonClick = "[] call life_fnc_spawnConfirm";
+			animTextureNormal = "#(argb,8,8,3)color(0.08,0.08,0.08,1)";
+			animTextureDisabled = "";
+			animTextureOver = "";
+			animTextureFocused = "";
+			animTexturePressed = "";
+			animTextureDefault = "";
+			size = 0.06;
+			class TextPos
+			{
+				left = "0.25 * 			(			((safezoneW / safezoneH) min 1.2) / 40)";
+				top = 0.020;
+				right = 0.005;
+				bottom = 0.0;
+			};
+			class Attributes
+			{
+				align = "center";
+				shadow = "false";
+			};
+		};
+	};
 };
