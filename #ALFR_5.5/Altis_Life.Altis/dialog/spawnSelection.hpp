57c868a6253ab26c884345f89a0444c51faca504
Sakzy <kitanovski@laposte.net>
2016-07-10 14:38:30 +0200

    #5.5_Test_New_Menu_spawn

--- #ALFR_5.5/Altis_Life.Altis/dialog/spawnSelection.hpp
+++ #ALFR_5.5/Altis_Life.Altis/dialog/spawnSelection.hpp
@@ -1,120 +1,108 @@
-class life_spawn_selection
-{
-	idd = 38500;
-	movingEnabled = false;
-	enableSimulation = true;
-	
-	class controlsBackground
-	{
-		class Logo: Life_RscPicture
-		{
-			idc = 1200;
-			text = "icons\AltisLifefr.jpg";
-			x = 0.725;
-			y = 0.063;
-			w = 0.3;
-			h = 0.1;
-		};
-		class Title: Life_RscText
-		{
-			idc = 1000;
-			text = "Lieux de Spawn";
-			x = 0.00883839;
-			y = 0.033468;
-			w = 0.6875;
-			h = 0.12;
-			sizeEx = 0.07;
-		};
-		class MapView : Life_RscMapControl 
-		{
-			idc = 38502;
-			x = 0.255; 
-			y = 0.16;
-			w = 0.749;
-			h = 0.84;
-			maxSatelliteAlpha = 0.75;
-			alphaFadeStartScale = 1.15;
-			alphaFadeEndScale = 1.29;
-		};
-		class listBackground: Life_RscText
-		{
-			idc = 100000;
-			x = 0.025;
-			y = 0.158;
-			w = 0.230;
-			h = 0.84;
-			colorBackground[] = {0.08,0.08,0.08,1};
-			text = "   ";
-		};		
-		class buttonBackground: Life_RscText
-		{
-			idc = 100000;
-			x = 0.7035;
-			y = 1;
-			w = 0.3;
-			h = 0.1;
-			colorBackground[] = {0.17,0.17,0.17,1};
-			text = "   ";
-		};
-	};
-	
-	class controls
-	{	
-		class SpawnPointList: Life_RscListNBox
-		{
-			idc = 38510;
-			coloumns[] = {0,0,0.9};
-			drawSideArrows = false;
-			idcLeft = -1;
-			idcRight = -1;
-			rowHeight = 0.1;
-			sizeEx = 0.05;
-			colorSelectBackground[] = {0.17,0.17,0.17,1};
-			colorSelectBackground2[] = {0.17,0.17,0.17,1};
-			colorSelect[] = {1,1,1,1};
-			colorSelect2[] = {1,1,1,1};
-			x = 0.025;
-			y = 0.16;
-			w = 0.230;
-			h = 0.84;
-			onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
-		};
-		
-		class spawnButton : Life_RscButtonMenu
-		{
-			idc = -1;
-			text = "Se Réveiller";
-			x = 0.7035;
-			y = 0.999;
-			w = 0.3;
-			h = 0.1;
-			colorBackground[] = {0.08,0.08,0.08,1};
-			colorBackground2[] = {0.08,0.08,0.08,1};
-			colorBackgroundFocused[] = {0.17,0.17,0.17,1};
-			color[] = {1,1,1,1};
-			colorFocused[] = {1,1,1,1};
-			color2[] = {1,1,1,1};
-			colorText[] = {1,1,1,1};
-			onButtonClick = "[] call life_fnc_spawnConfirm";
-			animTextureNormal = "#(argb,8,8,3)color(0.08,0.08,0.08,1)";
-			animTextureDisabled = "";
-			animTextureOver = "";
-			animTextureFocused = "";
-			animTexturePressed = "";
-			animTextureDefault = "";
-			size = 0.06;
-			class TextPos
-			{
-				left = "0.25 * 			(			((safezoneW / safezoneH) min 1.2) / 40)";
-				top = 0.020;
-				right = 0.005;
-				bottom = 0.0;
-			};
-			class Attributes
-			{
-				align = "center";
-				shadow = "false";
-			};
-		};
-	};
+class life_spawn_selection {
+    idd = 38500;
+    movingEnable = 0;
+    enableSimulation = 1;
+    onLoad = "call (profilenamespace getvariable ""LastSpawnPoint"");";
+  
+    class controlsBackground {
+      class life_RscTitleBackground: Life_RscText
+      {
+      	idc = -1;
+
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.397031 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])","(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+      };
+      class Title: Life_RscTitle
+      {
+      	idc = -1;
+
+      	text = "Spawn Selection"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.376406 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorText[] = {0.95,0.95,0.95,1};
+      };
+      class SpawnPointTitle: Title
+      {
+      	idc = 38501;
+      	style = 1;
+
+      	text = "Spawn Selection"; //--- ToDo: Localize;
+      	x = 0.04625 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.391875 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorText[] = {0.95,0.95,0.95,1};
+      };
+      class Background_1: Life_RscText
+      {
+      	idc = 2200;
+      	x = 0.041273 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.396876 * safezoneW;
+      	h = 0.341 * safezoneH;
+      	colorBackground[] = {0.188,0.188,0.188,0.5};
+      };
+      class MapView: Life_RscMapControl
+      {
+      	idc = 38502;
+      	maxSatelliteAlpha = 0.75;
+      	alphaFadeStartScale = 1.15;
+      	alphaFadeEndScale = 1.29;
+
+      	x = 0.2025 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.232031 * safezoneW;
+      	h = 0.341 * safezoneH;
+      };
+    };
+
+    class controls {
+      class SpawnPointList: Life_RscListNBox
+      {
+      	idc = 38510;
+      	coloumns[] = {0,0,0.9};
+      	drawSideArrows = 0;
+      	idcLeft = -1;
+      	idcRight = -1;
+      	rowHeight = 0.05;
+      	onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
+
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.341 * safezoneH;
+      };
+      class spawnButton: Life_RscButtonMenu
+      {
+      	onButtonClick = "[] call life_fnc_spawnConfirm";
+      	idc = 1006;
+
+      	text = "Spawn"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.753 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.033 * safezoneH;
+      	colorText[] = {1,1,1,1};
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])",0.5};
+      };
+      class ReturnToLobbyButton: Life_RscButtonMenu
+      {
+      	onButtonClick = "endmission'memes';";
+      	idc = 1007;
+
+      	text = "Return To Lobby"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.797 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.033 * safezoneH;
+      	colorText[] = {1,1,1,1};
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])",0.5};
+      };
+
+    };
 };
