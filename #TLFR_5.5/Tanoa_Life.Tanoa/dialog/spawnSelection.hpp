f214418f8b566b9663827c22a8c61465e2d3c78c
Sakzy <kitanovski@laposte.net>
2016-08-01 19:11:31 +0200

    Revert "#5.5_Try_NewSpawnMenuTanoa"
    This reverts commit 92c9f2f4350045ffc4f60ef0e2f84830a7c7504b.

--- #TLFR_5.5/Tanoa_Life.Tanoa/dialog/spawnSelection.hpp
+++ #TLFR_5.5/Tanoa_Life.Tanoa/dialog/spawnSelection.hpp
@@ -1,82 +1,108 @@
-class life_spawn_selection
-{
-	idd = 38500;
-	movingEnabled = 0;
-	enableSimulation = 1;
-	
-    class controlsBackground
-	{
-        class life_RscTitleBackground: Life_RscText
-        {
-	         idc = 1000;
-	         x = 0.0101577 * safezoneW + safezoneX;
-	         y = 0.0291999 * safezoneH + safezoneY;
-	         w = 0.170156 * safezoneW;
-	         h = 0.9438 * safezoneH;
-	         colorBackground[] = {0.294,0.286,0.286,0.8};
-        };
+class life_spawn_selection {
+    idd = 38500;
+    movingEnable = 0;
+    enableSimulation = 1;
+    onLoad = "call (profilenamespace getvariable ""LastSpawnPoint"");";
+  
+    class controlsBackground {
+      class life_RscTitleBackground: Life_RscText
+      {
+      	idc = -1;
 
-        class Title: Life_RscText
-        {
-	        idc = 1001;
-	        text = "Spawn Selection";
-	        x = 0.0132501 * safezoneW + safezoneX;
-	        y = 0.0402 * safezoneH + safezoneY;
-	        w = 0.164999 * safezoneW;
-	        h = 0.0242 * safezoneH;
-        };
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.397031 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])","(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+      };
+      class Title: Life_RscTitle
+      {
+      	idc = -1;
 
-        class MapView: Life_RscMapControl
-        {
-	        idc = 38502;
-	        x = 0.183407 * safezoneW + safezoneX;
-	        y = 0.0292 * safezoneH + safezoneY;
-	        w = 0.8085 * safezoneW;
-	        h = 0.9438 * safezoneH;
-	        maxSatelliteAlpha = 0.75;
-            alphaFadeStartScale = 1.15;
-            alphaFadeEndScale = 1.29;
-        };
+      	text = "Spawn Selection"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.376406 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorText[] = {0.95,0.95,0.95,1};
+      };
+      class SpawnPointTitle: Title
+      {
+      	idc = 38501;
+      	style = 1;
+
+      	text = "Spawn Selection"; //--- ToDo: Localize;
+      	x = 0.04625 * safezoneW + safezoneX;
+      	y = 0.368 * safezoneH + safezoneY;
+      	w = 0.391875 * safezoneW;
+      	h = 0.022 * safezoneH;
+      	colorText[] = {0.95,0.95,0.95,1};
+      };
+      class Background_1: Life_RscText
+      {
+      	idc = 2200;
+      	x = 0.041273 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.396876 * safezoneW;
+      	h = 0.341 * safezoneH;
+      	colorBackground[] = {0.188,0.188,0.188,0.5};
+      };
+      class MapView: Life_RscMapControl
+      {
+      	idc = 38502;
+      	maxSatelliteAlpha = 0.75;
+      	alphaFadeStartScale = 1.15;
+      	alphaFadeEndScale = 1.29;
+
+      	x = 0.2025 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.232031 * safezoneW;
+      	h = 0.341 * safezoneH;
+      };
     };
 
     class controls {
-        class spawnList: Life_RscListNbox
-        {
-	        idc = 38510;
-	        x = 0.0153125 * safezoneW + safezoneX;
-	        y = 0.071 * safezoneH + safezoneY;
-	        w = 0.159844 * safezoneW;
-	        h = 0.847 * safezoneH;
-            text = "";
-            sizeEx = 0.041;
-            coloumns[] = {0,0,0.9};
-            drawSideArrows = 1;
-            idcLeft = -1;
-            idcRight = -1;
-            rowHeight = 0.050;
-	        onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
-        };
+      class SpawnPointList: Life_RscListNBox
+      {
+      	idc = 38510;
+      	coloumns[] = {0,0,0.9};
+      	drawSideArrows = 0;
+      	idcLeft = -1;
+      	idcRight = -1;
+      	rowHeight = 0.05;
+      	onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
 
-        class spawnButton: Life_RscButtonMenu
-        {
-	        idc = -1;
-	        text = "Enter";
-	        x = 0.0153125 * safezoneW + safezoneX;
-	        y = 0.9246 * safezoneH + safezoneY;
-	        w = 0.159844 * safezoneW;
-	        h = 0.033 * safezoneH;
-	        style = "0x02";
-	        colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
-	        colorBackgroundActive[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",1};
-			colorBackgroundDisabled[] = {0.95,0.95,0.95,0};
-			offsetX = 0.003;
-			offsetY = 0.003;
-			offsetPressedX = 0.002;
-			offsetPressedY = 0.002;
-			colorShadow[] = {0,0,0,1};
-			colorBorder[] = {0,0,0,0};
-			borderSize = 0.008;
-            onButtonClick = "[] call life_fnc_spawnConfirm";
-        };
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.39 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.341 * safezoneH;
+      };
+      class spawnButton: Life_RscButtonMenu
+      {
+      	onButtonClick = "[] call life_fnc_spawnConfirm";
+      	idc = 1006;
+
+      	text = "Spawn"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.753 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.033 * safezoneH;
+      	colorText[] = {1,1,1,1};
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])",0.5};
+      };
+      class ReturnToLobbyButton: Life_RscButtonMenu
+      {
+      	onButtonClick = "endmission'memes';";
+      	idc = 1007;
+
+      	text = "Return To Lobby"; //--- ToDo: Localize;
+      	x = 0.0410937 * safezoneW + safezoneX;
+      	y = 0.797 * safezoneH + safezoneY;
+      	w = 0.159844 * safezoneW;
+      	h = 0.033 * safezoneH;
+      	colorText[] = {1,1,1,1};
+      	colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])",0.5};
+      };
+
     };
 };
