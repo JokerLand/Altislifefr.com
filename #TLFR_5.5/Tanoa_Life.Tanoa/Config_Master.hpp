281874fa7ea08d635610a5d507b4af1028c8ade0
JohnConry <d.quenot92@gmail.com>
2016-08-11 02:56:18 +0200

    #v5..5_fix temps respawn,

--- #TLFR_5.5/Tanoa_Life.Tanoa/Config_Master.hpp
+++ #TLFR_5.5/Tanoa_Life.Tanoa/Config_Master.hpp
@@ -45,7 +45,7 @@ class Life_Settings {
     donor_level = true; //Enable the donor level set in database (var = life_donorlevel; levels = 0,1,2,3,4,5). ATTENTION! Before enabling, read: https://www.bistudio.com/community/game-content-usage-rules & https://www.bistudio.com/monetization
     enable_fatigue = false; //Set to false to disable the ARMA 3 fatigue system.
     total_maxWeight = 24; //Static variable for the maximum weight allowed without having a backpack
-    respawn_timer = 240; //How many seconds a player should wait, before being able to respawn. Minimum 5 seconds.
+    respawn_timer = 120; //How many seconds a player should wait, before being able to respawn. Minimum 5 seconds.
     
     /* Channel 7 News Station Configurations */
     news_broadcast_cost = 100000; //Cost for a player to send a news station broadcast.
